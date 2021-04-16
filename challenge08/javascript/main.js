7bb1231ac5ee8539f180b7caeddbed53b52914ce
soransh <57911107+soransh-singh@users.noreply.github.com>
2021-04-16 09:40:26 +0530

    update

---
+++ challenge08/javascript/main.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  const navToggle = document.querySelector('.nav-toggle');
+  const nav = document.querySelector('.nav');
+
+  navToggle.addEventListener('click', () => {
+    nav.classList.toggle('nav--visible');
+    console.log("hello");
+  });
+}
